562de0af9c4e43f3e414cf4777f1bff421d9cdee
Lakshmi Venkata Vamshi Krishna <budagalavamshi28@gmail.com>
2026-06-13 16:48:18 +0530

    fix port for render deployment

--- server.js
+++ server.js
@@ -20,8 +20,9 @@ app.get('/{*splat}', (req, res) => {
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('MongoDB connected ✅');
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch(err => console.error(err));
